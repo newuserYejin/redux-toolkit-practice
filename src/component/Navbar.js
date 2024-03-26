3cb12d4d638df722b08ea6fbee6a58e3c4817220
jin <jin200126@gmail.com>
2024-03-27 01:41:42 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'H&M Home', 'Sale', '지속가능성']
+    return (
+        <div>
+            <div>
+                <div className='loginButton'>
+                    <FontAwesomeIcon icon={faUser} />
+                    <div>로그인</div>
+                </div>
+            </div>
+            <div className='navSection'>
+                <img
+                    width={100}
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png' />
+            </div>
+            <div className='menuArea'>
+                <ul className='menuList'>
+                    {menuList.map(menu => (
+                        <li>{menu}</li>
+                    ))}
+                </ul>
+                <div className='inputBox'>
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type='text' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img width={250} src="https://noona-hnm.netlify.app/pattern-jacket.jpeg" />
+            <div>Conscious choice</div>
+            <div>코트</div>
+            <div>\49000</div>
+            <div>신제품</div>
+        </div>
+    )
+}
+
+export default ProductCard
